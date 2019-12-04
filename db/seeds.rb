b161086d8309db282dc09f4caa15f1f3ca220932
Wenjun Wu <happy.wu1227@gmail.com>
2019-12-04 15:51:02 +0800

    scenario seeds fixed, and step activestorage changed to one attatched

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,15 +30,15 @@ s2 = Scenario.create!(
                 - Input your destination address
                 - Choose  a transportation'
 )
-s1.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/bus@3x.png"), filename: SecureRandom.hex(20))
+s2.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/bus@3x.png"), filename: SecureRandom.hex(20))
 
-s2 = Scenario.create!(
+s3 = Scenario.create!(
   title: 'Call your friends',
   description: 'You can talk with your family members with Wechat group call.
                 - Inside a group, open  the call
                 - Select members you want to talk with'
 )
-s1.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/call@3x.png"), filename: SecureRandom.hex(20))
+s3.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/call@3x.png"), filename: SecureRandom.hex(20))
 
 # groupchat demo seeds
 step1 = Step.create!(
@@ -56,7 +56,7 @@ step2 = Step.create!(
   instruction: 'Walktrough - long press this button',
   scenario: s1
 )
-step2.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-1.jpeg"), filename: SecureRandom.hex(20))
+step2.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-1.jpeg"), filename: SecureRandom.hex(20))
 
 step3 = Step.create!(
   category: 2,
@@ -64,7 +64,7 @@ step3 = Step.create!(
   instruction: 'Walkthrough - long press this button again',
   scenario: s1
 )
-step3.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-2.jpeg"), filename: SecureRandom.hex(20))
+step3.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-2.jpeg"), filename: SecureRandom.hex(20))
 
 step4 = Step.create!(
   category: 2,
@@ -72,7 +72,7 @@ step4 = Step.create!(
   instruction: 'Walkthrough - tap to continue',
   scenario: s1
 )
-step4.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-3.jpeg"), filename: SecureRandom.hex(20))
+step4.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-3.jpeg"), filename: SecureRandom.hex(20))
 
 step5 = Step.create!(
   category: 2,
@@ -80,7 +80,7 @@ step5 = Step.create!(
   instruction: 'Walkthrough - tap to continue',
   scenario: s1
 )
-step5.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-4.jpeg"), filename: SecureRandom.hex(20))
+step5.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-4.jpeg"), filename: SecureRandom.hex(20))
 
 step6 = Step.create!(
   category: 2,
@@ -88,7 +88,7 @@ step6 = Step.create!(
   instruction: 'Walkthrough - tap to continue',
   scenario: s1
 )
-step6.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-5.jpeg"), filename: SecureRandom.hex(20))
+step6.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-5.jpeg"), filename: SecureRandom.hex(20))
 
 step7 = Step.create!(
   category: 2,
@@ -96,7 +96,7 @@ step7 = Step.create!(
   instruction: 'Walkthrough - tap to continue',
   scenario: s1
 )
-step7.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-6.jpeg"), filename: SecureRandom.hex(20))
+step7.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-6.jpeg"), filename: SecureRandom.hex(20))
 
 step8 = Step.create!(
   category: 2,
@@ -104,22 +104,40 @@ step8 = Step.create!(
   instruction: 'Walkthrough - tap to continue',
   scenario: s1
 )
-step8.images.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-7.jpeg"), filename: SecureRandom.hex(20))
+step8.image.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Groupcall-7.jpeg"), filename: SecureRandom.hex(20))
 
 # groupchat practice seeds
-step4 = Step.create!(
+step9 = Step.create!(
   category: 3,
   priority: 1,
   instruction: 'Practice - tap to continue again',
   scenario: s1
 )
 
-step5 = Step.create!(
+step10 = Step.create!(
   category: 3,
   priority: 2,
   instruction: 'Practice - End of practice',
   scenario: s1
 )
+
+# map demo seeds
+step11 = Step.create!(
+  category: 1,
+  priority: 1,
+  instruction: 'Demo',
+  scenario: s1
+)
+step11.video.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Map.mp4"), filename: SecureRandom.hex(20))
+
+# didi demo seeds
+step12 = Step.create!(
+  category: 1,
+  priority: 1,
+  instruction: 'Demo',
+  scenario: s1
+)
+step12.video.attach(io: open("http://q1xs0nzvt.bkt.clouddn.com/Didi.mp4"), filename: SecureRandom.hex(20))
 
 puts "Created #{User.count} User"
 puts "Created #{Scenario.count} scenarios"
