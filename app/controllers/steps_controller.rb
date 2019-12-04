59dd47f5104486318ad33fd93c0fd79ad7bbd1ce
huifang boo <huifangb@gmail.com>
2019-12-04 18:27:21 +0800

    corrected step show controller issues with rendering information for one scenario

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -3,29 +3,29 @@ class StepsController < ApplicationController
 
   def index
     # raise
-    @scenario = Scenario.find(params[:scenario_id])
+    # @scenario = Scenario.find(params[:scenario_id])
 
-    @c1 = Step.where(category: 1).first
-    @c2 = Step.where(category: 2).first
-    @c3 = Step.where(category: 3).first
+    # @c1 = Step.where(category: 1).first
+    # @c2 = Step.where(category: 2).first
+    # @c3 = Step.where(category: 3).first
   end
 
   def show
-
     @scenario = @step.scenario
-    @c1 = Step.where(category: 1).first
-    @c2 = Step.where(category: 2).first
-    @c3 = Step.where(category: 3).first
+    # @c1 = Step.where(category: 1, scenario_id: @scenario.id).first
+    # @c2 = Step.where(category: 2, scenario_id: @scenario.id).first
+    # @c3 = Step.where(category: 3, scenario_id: @scenario.id).first
     # raise
     @record = Record.new
     case @step.category
     when 1
-      @steps = Step.where(category: 1)
+      @steps = Step.where(category: 1, scenario_id: @scenario.id)
     when 2
-      @steps = Step.where(category: 2)
+      @steps = Step.where(category: 2, scenario_id: @scenario.id)
     when 3
-      @steps = Step.where(category: 3)
+      @steps = Step.where(category: 3, scenario_id: @scenario.id)
     end
+    # raise
   end
 
   def update
