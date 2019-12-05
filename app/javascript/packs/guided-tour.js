47ead19da1edd4d9d9f488068aca26ea959903b4
Wenjun Wu <happy.wu1227@gmail.com>
2019-12-05 16:35:49 +0800

    still working on the js package

--- app/javascript/packs/guided-tour.js
+++ app/javascript/packs/guided-tour.js
@@ -1,4 +1,25 @@
-var anno1 = new Anno({
-  target : 'pre:first',
-  content: 'This is an annotation'
-})
+// var anno1 = new Anno({
+//   target : 'pre:first',
+//   content: 'This is an annotation'
+// })
+
+
+var tour = new Tour({
+  steps: [
+  {
+    element: "#my-element",
+    title: "Title of my step",
+    content: "Content of my step"
+  },
+  {
+    element: "#my-other-element",
+    title: "Title of my step",
+    content: "Content of my step"
+  }
+]});
+
+// Initialize the tour
+tour.init();
+
+// Start the tour
+tour.start();
