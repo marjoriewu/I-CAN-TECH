f09eeb601023d1bd85a80c7fd00b530c00ba908c
Wenjun Wu <happy.wu1227@gmail.com>
2019-12-06 16:40:14 +0800

    fixing the git problem

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Step.all.destroy_all
+Badge.all.destroy_all
 Record.all.destroy_all
 Scenario.all.destroy_all
 User.all.destroy_all
