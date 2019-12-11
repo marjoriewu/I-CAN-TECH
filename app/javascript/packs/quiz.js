81587f8132a542bdc3aeccc9fabfff6f9ffca51c
Maggie Gong <meijic@outlook.com>
2019-12-11 15:16:46 +0800

    make the quiz choices and alerts fixed to top on scroll

--- app/javascript/packs/quiz.js
+++ app/javascript/packs/quiz.js
@@ -1,6 +1,15 @@
 // The number of the correct answer:
 const correctAnswer = document.querySelector('#right-answer').innerText;
 
+// Quiz content:
+const quizContent = document.querySelector('#quiz-content');
+
+// The quiz options bar:
+const quizOptions = document.querySelector('.quiz-options');
+
+// Empty div that takes up the space after quiz options bar has been fixed:
+const quizOptionsGhost = document.querySelector('.quiz-options-empty');
+
 // The three clickable options:
 const optionItems = document.querySelectorAll('.quiz-option-items');
 
@@ -10,6 +19,12 @@ const correctAlert = document.querySelector('.quiz-alert-correct');
 // Incorrect answer alert:
 const incorrectAlert = document.querySelector('.quiz-alert-incorrect');
 
+// Empty alert that takes up the space after alert has been fixed:
+const alertGhost = document.querySelector('.alert-empty');
+
+// Variable that tracks if alert is on:
+let alertOn = false;
+
 optionItems.forEach((item) => {
   item.addEventListener('click', () => {
     // Remove any previous alert if exists
@@ -18,9 +33,33 @@ optionItems.forEach((item) => {
     if (item.innerText == correctAnswer) {
       item.classList.add('correct-answer');
       correctAlert.classList.remove('d-none');
+      alertOn = true;
     } else {
       item.classList.add('incorrect-answer');
       incorrectAlert.classList.remove('d-none');
+      alertOn = true;
     }
   });
+});
+
+window.addEventListener("scroll", function() {
+
+  if (this.scrollY > (quizContent.clientHeight + 96)) {
+    quizOptions.classList.add("quiz-options-fixed");
+    quizOptionsGhost.classList.remove('d-none');
+    correctAlert.classList.add('quiz-alert-fixed');
+    incorrectAlert.classList.add('quiz-alert-fixed');
+    if (alertOn) {
+      alertGhost.classList.remove('d-none');
+    }
+  } else {
+    quizOptions.classList.remove("quiz-options-fixed");
+    quizOptionsGhost.classList.add('d-none');
+    correctAlert.classList.remove('quiz-alert-fixed');
+    incorrectAlert.classList.remove('quiz-alert-fixed');
+    if (alertOn) {
+      alertGhost.classList.add('d-none');
+    }
+  }
+
 });
