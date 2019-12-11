5c80a1202cf2e0420749ba67ac8434ccea4d1a19
Wenjun Wu <happy.wu1227@gmail.com>
2019-12-11 14:47:19 +0800

    trial page styling fixed

--- db/schema.rb
+++ db/schema.rb
@@ -95,8 +95,6 @@ ActiveRecord::Schema.define(version: 2019_12_11_025839) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name"
-    t.string "country_code"
-    t.text "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
